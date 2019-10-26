9ea48d638412881dfae466387a09dbe1b85e81d4
mac <mac@macdeMacBook-Pro.local>
2019-10-26 23:58:22 +0800

    添加了根据ip获取标题的功能

--- web_title.py
+++ web_title.py
@@ -10,6 +10,7 @@ import chardet
 import aiohttp
 import ssl
 import csv
+import IPy
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -23,7 +24,7 @@ class WebTitle:
     def init_queue(self):
         queue = asyncio.Queue()
         for url in self.urls:
-           queue.put_nowait(url)
+            queue.put_nowait(url)
         return queue
 
     def get_title_from_html(self, html):
@@ -38,6 +39,7 @@ class WebTitle:
     async def get_title(self):
         while True:
             url = await self.queue.get()
+            print('get title for {}'.format(url))
             try:
                 async with aiohttp.ClientSession() as session:
                     async with session.get(url, timeout=3, ssl=ssl.SSLContext()) as resp:
@@ -46,7 +48,7 @@ class WebTitle:
                 print('{}:{}'.format(url,title))
                 self.result[url] = title
             except Exception as e:
-                pass                
+                print('{} has error: {} '.format(url,str(e)))                
             self.queue.task_done()
 
     async def start_task(self):
@@ -57,12 +59,12 @@ class WebTitle:
             tasks.append(task)
 
         await self.queue.join()
-        
+
         for task in tasks:
             task.cancel()
 
         await asyncio.gather(*tasks, return_exceptions=True)
-        
+
 
     def start(self):
         asyncio.run(self.start_task())
@@ -81,34 +83,66 @@ def parse_args():
     parser = argparse.ArgumentParser(description='A tool that can get title for domains or urls')
     parser.add_argument('-d','--domain', metavar='domain.txt', dest='domain_file', type=str, help=u'domain to get title')
     parser.add_argument('-u','--url', metavar='url.txt', dest='url_file', type=str, help=u'urls to get title')
+    parser.add_argument('-i','--ip', metavar='ip.txt', dest='ip_file', type=str, help=u'ips to get title')
     parser.add_argument('-t','--thread', metavar='20', dest='thread_count', type=int, default=20,help=u'threads to get title')
     parser.add_argument('-o','--outfile', metavar='result.txt', dest='outfile', type=str, default='result.csv',help=u'file to result')
     args = parser.parse_args()
-    if args.url_file == None and args.domain_file == None:
+    if args.url_file == None and args.domain_file == None and args.ip_file == None:
         parser.print_help()
         sys.exit()
     return args
 
 
 def main():
-    args = parse_args()
-    urls = []
-    if args.domain_file:
-        with open(args.domain_file) as f:
-            domains = f.readlines()
-        for domain in domains:
-            urls.append('http://' + domain.strip())
-            urls.append('https://' + domain.strip())
+    try:
+        args = parse_args()
+        urls = []
 
-    if args.url_file:
-        with open(args.url_file) as f:
-            urls2 = f.readlines()
-        for url in urls2:
-            urls.append(url.strip())
-    web_title = WebTitle(urls, args.thread_count)
-    web_title.start()
-    web_title.write_result(args.outfile)
+        if args.domain_file:
+            with open(args.domain_file) as f:
+                domains = f.readlines()
+            for domain in domains:
+                domain = domain.strip()
+                if domain != '':
+                    urls.append('http://' + domain)
+                    urls.append('https://' + domain)
+
+        if args.url_file:
+            with open(args.url_file) as f:
+                urls2 = f.readlines()
+            for url in urls2:
+                url = url.strip()
+                if url != '':
+                    urls.append(url)
+
+        if args.ip_file:
+            with open(args.ip_file) as f:
+                ips = f.readlines()
+            for ip in ips:
+                ip = ip.strip()
+                if ip != '':
+                    cidr_ip = IPy.IP(ip)
+                    for i in cidr_ip:
+                        urls.append('http://' + str(i))
+                        urls.append('https://' + str(i))
+
+        web_title = WebTitle(urls, args.thread_count)
+        web_title.start()
+        web_title.write_result(args.outfile)
+    except Exception as e:
+        print(e)
+
+def banner():
+    print('''                                             
+  __           _                  _       _   _ _   _      
+ / _| __ _ ___| |_  __      _____| |__   | |_(_) |_| | ___ 
+| |_ / _` / __| __| \ \ /\ / / _ \ '_ \  | __| | __| |/ _ \ 
+|  _| (_| \__ \ |_   \ V  V /  __/ |_) | | |_| | |_| |  __/
+|_|  \__,_|___/\__|   \_/\_/ \___|_.__/   \__|_|\__|_|\___|
+                                                           ''')
+
 
 if __name__ == '__main__':
+    banner()
     main()
 
